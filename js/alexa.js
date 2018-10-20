f2771e75c8e97420c23fa9b4696f33802293efb4
Srikanth <snandiraju@paypal.com>
2018-10-20 16:59:51 -0700

    check now

--- js/alexa.js
+++ js/alexa.js
@@ -1,9 +1,9 @@
 // ------------- PUBSUB KEY --------------------
-var pubnub = PubNub.init({
+var pubnub = PUBNUB.init({
 	publish_key : 'pub-c-04318e42-5630-4853-bd7f-eca169eafed8',
-	subscribe_key : 'sub-c-8f0cc37c-c03d-11e3-b230-02ee2ddab7fe',
-	ssl:true
+	subscribe_key : 'sub-c-8f0cc37c-c03d-11e3-b230-02ee2ddab7fe'
 });
+
 pubnub.subscribe({
 	channel : "digitalmall_alexa",
 	message : function(m) {
