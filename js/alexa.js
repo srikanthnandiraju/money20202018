e4e5866d07e5b2493c0feeba486ee6b20a55060b
Srikanth <snandiraju@paypal.com>
2018-10-20 17:11:29 -0700

    flag

--- js/alexa.js
+++ js/alexa.js
@@ -1,13 +1,13 @@
 // ------------- PUBSUB KEY --------------------
 var pubnub = PUBNUB.init({
-	publish_key : 'pub-c-04318e42-5630-4853-bd7f-eca169eafed8',
-	subscribe_key : 'sub-c-8f0cc37c-c03d-11e3-b230-02ee2ddab7fe',
-	ssl:true
+	publish_key: 'pub-c-04318e42-5630-4853-bd7f-eca169eafed8',
+	subscribe_key: 'sub-c-8f0cc37c-c03d-11e3-b230-02ee2ddab7fe',
+	ssl: true
 });
 
 pubnub.subscribe({
-	channel : "digitalmall_alexa",
-	message : function(m) {
+	channel: "digitalmall_alexa",
+	message: function (m) {
 		m = JSON.parse(m);
 		console.log("GOT NOTIFICATION - > " + m.screen);
 		handleData(m);
